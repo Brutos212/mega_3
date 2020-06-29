1234c994ccf87d0133eb192e5464ca8731740b8f
brutos <brutos212@gmail.com>
2020-06-29 00:53:15 -0300

    adicionando recurso de listagem de tarefa

---
+++ test/test_gerenciador.py
@@ -0,0 +1,37 @@
+
+
+from starlette.status import HTTP_200_OK
+from starlette.testclient import TestClient
+
+from gerenciador_tarefas.gerenciador import app, TAREFAS
+
+
+def test_quando_listar_tarefas_devo_retornar_codigo_de_status_ok():
+    cliente = TestClient(app)
+    resposta = cliente.get("/tarefas")
+    assert resposta.status_code == HTTP_200_OK
+
+
+def test_quando_listar_tarefas_formato_deve_ser_json():
+    cliente = TestClient(app)
+    resposta = cliente.get("/tarefas")
+    assert resposta.headers['Content-Type'] == "application/json"
+
+
+def test_quando_listar_tarefas_retorno_deve_ser_lista():
+    cliente = TestClient(app)
+    resposta = cliente.get("/tarefas")
+    assert isinstance(resposta.json(), list)
+
+def test_deve_listar_tarefas():
+    tarefa = {
+        "id": 1,
+        "titulo": "titulo",
+        "descricao": "descricao",
+        "estado": "finalizado",
+    }
+    TAREFAS.append(tarefa)
+    cliente = TestClient(app)
+    resposta = cliente.get("/tarefas")
+    assert resposta.json() == [tarefa]
+    TAREFAS.clear()
